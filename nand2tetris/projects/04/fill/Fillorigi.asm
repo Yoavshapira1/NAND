d171232f026e457e1c06e3358f0352c895c6d7b1
Yoavshapira1 <62549504+Yoavshapira1@users.noreply.github.com>
2021-10-21 15:51:49 +0300

    FILL

--- nand2tetris/projects/04/fill/Fillorigi.asm
+++ nand2tetris/projects/04/fill/Fillorigi.asm
@@ -11,4 +11,60 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@8192
+D=A
+@limit
+M=D
+
+@color
+M=0
+(WHILE)
+    @color
+    D=!M
+    @KBD
+    D=D&M
+    @ELSE
+    D;JEQ
+    @color
+    M=-1
+    @PAINT
+    0;JMP
+
+    (ELSE)
+        @KBD
+        D=!M
+        @color
+        D=D&M
+        @CONTINUE
+        D;JEQ
+        @color
+        M=0
+        @PAINT
+        0;JMP
+
+    (CONTINUE)
+    @WHILE
+    0;JMP
+
+(PAINT)
+    @j
+    M=0
+    (LOOP)
+        @limit
+        D=M
+        @j
+        D=D-M
+        @WHILE
+        D;JLT
+
+        @SCREEN
+        D=A
+        @j
+        A=D+M
+        M=!M
+        @j
+        M=M+1
+        @LOOP
+        0;JMP
+
+
