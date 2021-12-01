30c17fe5b7b073477fbf9bc6e689a85e71945fb7
Nitai Seri <Nitai.ns@gmail.com>
2021-12-01 18:45:43 +0200

    updade main and parser complete write_init, write_function, write_return

--- nand2tetris/projects/08/Main.py
+++ nand2tetris/projects/08/Main.py
@@ -4,6 +4,7 @@ https://www.nand2tetris.org (Shimon Schocken and Noam Nisan, 2017)
 and as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0 
 Unported License (https://creativecommons.org/licenses/by-nc-sa/3.0/).
 """
+import glob
 import os
 import sys
 import typing
@@ -23,8 +24,14 @@ def run_through_vm_code(parser, code_write):
             code_write.write_arithmetic(parser.arg1())
         elif command_type == "C_PUSH" or command_type == "C_POP":
             code_write.write_push_pop(command_type, parser.arg1(), parser.arg2())
-        if command_type == "C_BRANCHING":
+        elif command_type == "C_BRANCHING":
             code_write.write_branching(*parser.loop_labels())
+        elif command_type == "C_FUNCTION":
+            code_write.write_function(*parser.func_args())
+        elif command_type == "C_CALL":
+            code_write.write_call(*parser.func_args())
+        elif command_type == "C_RETURN":
+            code_write.write_return()
         parser.advance()
 
 
@@ -36,10 +43,15 @@ def translate_file(input_file: typing.TextIO, output_file: typing.TextIO) -> Non
         output_file (typing.TextIO): writes all output to this file.
     """
     input_filename, input_extension = os.path.splitext(os.path.basename(input_file.name))
-    parser = Parser(input_file)         # Parser Object
-    code_writer = CodeWriter(output_file)    # Write the hack code
-    code_writer.set_file_name(input_filename)
-    run_through_vm_code(parser, code_writer)
+    code_writer = CodeWriter(output_file)   # Write the hack code
+    if os.path.isdir(input_filename):
+        files_list = glob.glob("*.vm")
+    else:
+        files_list = [input_file]
+    for filename in files_list:
+        parser = Parser(filename)  # Parser Object
+        code_writer.set_file_name(input_filename)
+        run_through_vm_code(parser, code_writer)
     code_writer.close()
 
 if "__main__" == __name__:
