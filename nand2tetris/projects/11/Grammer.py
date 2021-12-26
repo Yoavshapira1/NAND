32268d878e91c5831b8da4cd0a044b359cefe586
Yoavshapira1 <62549504+Yoavshapira1@users.noreply.github.com>
2021-12-26 18:02:55 +0200

    TESTS PASSED

---
+++ nand2tetris/projects/11/Grammer.py
@@ -0,0 +1,70 @@
+import re
+
+
+KEYWORDS = "(class|method|function|constructor|int|" \
+           "boolean|char|void|var|static|field|let|do|" \
+           "if|else|while|return|true|false|null|this)"
+
+SYMBOLS = "(\{|\}|\(|\)|\[|\]|\.|,|;|\+|\-|\*|\/|&|\||<|>|=|~|\^|#)"
+
+INTEGER = "[0-9]+"
+
+STRING = "STRING"
+
+IDENTIFIER = "[a-zA-Z_][\w]*"
+
+KEYWORD = "keyword"
+
+SYMBOL = "symbol"
+
+INTEGER_CONSTANT = "integerConstant"
+
+STRING_CONSTANT = "stringConstant"
+
+TOKEN_TYPES_REGEX = {KEYWORDS: KEYWORD,
+                     SYMBOLS: SYMBOL,
+                     INTEGER: INTEGER_CONSTANT,
+                     STRING: STRING_CONSTANT,
+                     IDENTIFIER: "identifier"}
+
+
+### dont know if needed yet so didnt check all below
+
+# WHITESPACE = "\\s+"
+#
+# TERM = INTEGER_CONSTANT + "|" + STRING_CONSTANT + "|" + KEYWORDS + "|"
+#
+# CLASS_NAME = IDENTIFIER
+#
+# SUBROUTINE_NAME = IDENTIFIER
+#
+# VAR_NAME = IDENTIFIER
+#
+# OP = "(\\+|\\-|\\*|\\/|\\&|\\||\\<|\\>|\\=)"
+#
+# EXPRESSION = TERM + "(" + OP + TERM + ")*"
+#
+# ARRAY = VAR_NAME + "[" + EXPRESSION + "]"
+
+SPECIAL_SYMBOLS = {"&" : "&amp;", "<" : "&lt;", ">" : "&gt;"}
+STATEMENTS = ['let', 'while', 'if', 'do', 'return']
+OP = ['+', '-', '*', '/', '&', '|', '>', '<', '=']
+UNARY_OP = ['-', '~', '#', '^']
+CONSTANTS = [STRING_CONSTANT, INTEGER_CONSTANT, KEYWORD]
+SUBROUTINE = ["method", "function", "constructor"]
+CLASS_VAR = ["static", "field"]
+
+
+##PROBLEMS:
+# STRING - except new line
+# term + 5 - can be let + 5?
+
+if __name__ == '__main__':
+    txt = ""
+    x = re.sub(r'\([^)]*\)', '', txt)
+
+    if x:
+        print("YES! We have a match!")
+        print(x)
+    else:
+        print("No match")
